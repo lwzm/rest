b7853a22e04a316dab78734aa1d55fdc3f655435
q <lwzm@qq.com>
2017-11-25 23:09:36 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+
+const path = require('path')
+
+module.exports = {
+    watch: false,
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            'env',
+                        ],
+                    },
+                },
+            },
+        ],
+
+    },
+}
