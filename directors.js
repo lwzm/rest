367dbb93fa3d2dbcd7d1819f0448c0da422acbd3
q <lwzm@qq.com>
2018-08-23 20:49:20 +0800

    directors improved

--- directors.js
+++ directors.js
@@ -1,15 +1,16 @@
-function test(Restangular, $state, notification) {
+function test($state, notification) {
     return {
         scope: {
             entry: "&",
         },
         link: function(scope, element, attrs) {
             const entry = scope.entry()
-            scope.values = entry.values
+            const values = entry.values
+
+            scope.values = values
             scope.click = function() {
-                Restangular
-                    .one(entry._entityName, entry.values.id)
-                    .patch({name: entry.values.name + "!"})
+                values
+                    .patch({name: values.name + "!"})
                     .then(() => $state.reload())
                     .then(() => notification.log("OK", {addnCls: 'humane-flatty-success'}))
                     .catch(e => notification.log('Error', {addnCls: 'humane-flatty-error'}) && console.error(e))
@@ -17,13 +18,13 @@ function test(Restangular, $state, notification) {
             //console.log(scope, element, attrs)
         },
         template: `
-			<a ng-if="!values.name.endsWith('!')" class="btn btn-outline btn-success btn-sm" ng-click="click()">
+			<a ng-if="!values.name.endsWith('!')" class="btn btn-outline btn-success btn-xs" ng-click="click()">
 				<span class="glyphicon glyphicon-thumbs-up"></span>
 			</a>
 		`
     }
 }
-test.$inject = ['Restangular', '$state', 'notification']
+test.$inject = ['$state', 'notification']
 
 
 function directive(App) {
