a793b55185c9ecb6c6d4f10b710c6795f67af39e
q <lwzm@qq.com>
2018-11-10 22:47:16 +0800

    implement a subset of postgrest
    via pony orm and tornado

---
+++ app.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+
+import json
+import urllib.parse
+
+import tornado.web
+
+import entities
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def write_json(self, obj, default=str):
+        self.set_header("Content-Type", "application/json; charset=UTF-8")
+        self.write(json.dumps(obj, default=default, ensure_ascii=False,
+                              separators=(",", ":")))
+
+    @property
+    def kwargs(self):
+        if not hasattr(self, "_kwargs"):
+            self._kwargs = tornado.util.ObjectDict(
+                urllib.parse.parse_qsl(self.request.query))
+        return self._kwargs
+
+    @property
+    def json(self):
+        if not hasattr(self, "_json"):
+            self._json = json.loads(self.request.body)
+        return self._json
+
+
+op_map = {
+    "eq": "=",
+    "gt": ">",
+    "lt": "<",
+    "like": "like",
+}
+
+args_not_used = {"order", "select", }
+
+
+def magic_it(Entity):
+    from datetime import datetime, date
+    from pony import orm
+    from pony.converting import str2datetime, str2date
+
+    converts = {}
+
+    for i in Entity._attrs_:
+        t = i.py_type
+        conv = json.loads
+        if t is datetime:
+            conv = str2datetime
+        elif t is date:
+            conv = str2date
+        elif t is str:
+            conv = lambda x: x
+        converts[i.column] = conv
+
+    #orm.sql_debug(1)
+    class Handler(BaseHandler):
+
+        def _select(self):
+            filters = []
+            args = []
+
+            for k, v in urllib.parse.parse_qsl(self.request.query):
+                if k in args_not_used:
+                    continue
+                op, _, value = v.partition(".")
+                if not value:
+                    continue
+
+                value = converts[k](value)
+                op = op_map[op]
+                idx = len(args)
+                filters.append(f"{k} {op} $(args[{idx}])")
+                args.append(value)
+
+            #print(filters, args)
+
+            q = Entity.select()
+            if filters:
+                q = q.filter(lambda x: orm.raw_sql(" and ".join(filters)))
+
+            order = self.get_argument("order", None)
+            if order:
+                field, _, sc = order.partition(".")
+                sc = sc or "asc"
+                q = q.order_by(getattr(getattr(Entity, field), sc))
+
+            return q
+
+        def get(self):
+            headers = self.request.headers
+            single = ".object" in headers.get("Accept", "")
+            if single:
+                start, stop = 0, 0
+            else:
+                try:
+                    start, stop = map(int, headers["Range"].split("-"))
+                except KeyError:
+                    start, stop = 0, 99
+            exact = "count=exact" in headers.get("Prefer", "")
+            count = "*"
+
+            with orm.db_session:
+                q = self._select()
+                if exact:
+                    count = q.count()
+                lst = [i.to_dict() for i in q[start:stop + 1]]
+
+            self.set_header("Content-Range", f"{start}-{stop}/{count}")
+
+            if single:
+                self.write_json(lst[0])
+            else:
+                self.write_json(lst)
+
+        def post(self):
+            #print(self.json)
+            with orm.db_session:
+                Entity(**self.json)
+
+        def patch(self):
+            #print(self.json)
+            if not self.json:
+                return
+            with orm.db_session:
+                single, = self._select()
+                single.set(**self.json)
+
+        def delete(self):
+            with orm.db_session:
+                single, = self._select()
+                single.delete()
+
+    name = Entity.__name__.lower()
+    return f"/{name}", Handler
+
+
+handlers = [
+    magic_it(i)
+    for i in entities.Entity.__subclasses__()
+]
+
+app = tornado.web.Application(handlers)
+
+
+if __name__ == '__main__':
+    app.listen(8000)
+    import tornado.ioloop
+    tornado.ioloop.IOLoop.current().start()
+else:
+    import tornado.wsgi
+    application = tornado.wsgi.WSGIAdapter(app)
---
+++ entities.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+
+"""
+money patch first
+converting.str2datetime = my custom str2datetime must be first
+"""
+# money patch begin
+from datetime import datetime, date
+
+import pendulum
+import pony.converting
+
+def str2datetime(s):
+    dt = pendulum.parse(s)
+    return datetime.fromtimestamp(dt.timestamp())
+
+def str2date(s):
+    dt = pendulum.parse(s)
+    return date.fromtimestamp(dt.timestamp())
+
+pony.converting.str2datetime = str2datetime
+pony.converting.str2date = str2date
+
+import pony.orm
+assert pony.orm.dbapiprovider.str2datetime is str2datetime
+assert pony.orm.dbapiprovider.str2date is str2date
+# money patch end
+
+from pony.orm import (
+    Database,
+    PrimaryKey,
+    Required,
+    Optional,
+    Set,
+    Json,
+    db_session,
+    sql_debug,
+)
+
+
+#sql_debug(True)
+
+db = Database()
+Entity = db.Entity
+
+
+class Person(Entity):
+    #id = PrimaryKey(int, auto=True)
+    name = Required(str, 32)
+    age = Required(int)
+    dt = Optional(datetime)
+    data = Optional(Json)
+    cars = Set(lambda: Car)
+
+
+class Car(Entity):
+    #id = PrimaryKey(int, auto=True)
+    make = Required(str, 64)
+    model = Optional(str, 64, nullable=True)
+    owner = Required(lambda: Person)
+
+
+class Test(Entity):
+    f = Optional(float)
+    b = Optional(bool)
+    d = Optional(datetime)
+    t = Optional(date)
+    j = Optional(Json)
+
+
+db.bind(provider='sqlite', filename='test.db', create_db=True)
+db.generate_mapping(create_tables=True)
+
+
+formats_for_js = {
+    str: "string",
+    int: "number",
+    float: "float",
+    bool: "boolean",
+    datetime: "datetime",
+    date: "date",
+    Json: "json",
+}
+
+
+def init():
+    with db_session:
+        p1 = Person(name='John', age=20, dt=datetime.now())
+        p2 = Person(name='Mary', age=22, data=[{1:2}])
+        p3 = Person(name='Bob', age=30)
+        Car(make='Toyota', model='Prius', owner=p2)
+        Car(make='Ford', model='Explorer', owner=p3)
+        Test(t=date.today())
+
+
+def export():
+    lst = []
+    pks = {}
+    tables = Entity.__subclasses__()
+
+    with open("entities-patch.yaml") as f:
+        import yaml
+        patch = yaml.load(f)
+
+    # 1
+    for table in tables:
+        tableName = table.__name__.lower()
+        tablePatch = patch.get(tableName, {})
+        pk = None
+        fs = []
+        for column in table._attrs_:
+            if not column.column:
+                continue
+            assert column.py_type, column
+            columnName = column.column
+            if column.is_pk:
+                pk = columnName
+                pks[table] = {
+                    "tableName": tableName,
+                    "columnName": columnName,
+                }
+            py_type = column.py_type
+            type = formats_for_js.get(py_type, py_type)
+            if type == "string" and not column.args:
+                type = "text"
+            o = {
+                "columnName": columnName,
+                "type": type,
+            }
+            o.update(tablePatch.pop(columnName, {}))
+            fs.append(o)
+
+        t = {
+            "tableName": tableName,
+            "primaryKey": pk,
+            "fs": fs,
+        }
+        t.update(tablePatch)
+        lst.append(t)
+
+    # 2
+    for i in lst:
+        for f in i["fs"]:
+            t = f["type"]
+            if not isinstance(t, str):
+                assert issubclass(t, Entity), t
+                f["foreignKey"] = pks[f.pop("type")]
+
+    import json
+    print("export default ", json.dumps(lst,  indent=4, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    init()
+    export()
