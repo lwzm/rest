030459a85b173df37e64bec366fb1a1e7c00e240
q <lwzm@qq.com>
2018-12-05 12:54:13 +0800

    clean php

--- convert_pony_entities.py
+++ convert_pony_entities.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 import json
+import sys
+
 import requests
 
-def export():
-    rsp = requests.get("http://localhost:18000/")
+
+def export(api="http://localhost:18000/"):
+    rsp = requests.get(api)
     print("export default ", json.dumps(rsp.json(),  indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    export()
+    export(*sys.argv[1:])
